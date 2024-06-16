ae5550f2fff86e14db541d05dea1e1b0b67f6e8f
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2024-06-17 02:28:43 +1200

    #3 Create About Page (install) npm install framer-motion --save npm install --save-dev @babel/plugin-proposal-private-property-in-object npm install --save-dev react-app-rewired npm install --save-dev crypto-browserify stream-browserify assert stream-http https-browserify os-browserify url buffer process npm install --save sass npm i react-type-animation --legacy-peer-deps
    (add)
    .env file to put some variables such as name and email.
    fadeProps to use fade animation through framer-motion.
    custom.scss
    config-overrides.js to resolve errors for webpack cofig.
    typeAnimation by react-type-animation
    
    (fix)
    webpack issues with .env file

---
+++ src/components/fadeProps.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+
+const Fade = ({ children, isActive, movement }) => {
+    const dy = movement;
+    return (
+        <AnimatePresence>
+            {isActive && (
+                <motion.div
+                    initial={{ opacity: 0, y: dy }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, x: "100%" }}
+                    transition={{ duration: 1 }}
+                >
+                    {children}
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+};
+
+export default Fade;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import About from "./About";
+
+export default function Home() {
+    return (
+        <>
+            <div className="main-content mx-auto img-fluid contianer-fluid">
+                <About />
+            </div>
+        </>
+    );
+}
